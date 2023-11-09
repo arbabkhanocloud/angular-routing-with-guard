7175eeb4e4eb67cf3b312190239d661b858a89b1
Bilal Ashraf <bilalashraf1710@gmail.com>
2023-11-09 14:02:48 +0500

    feat: added login Form and some validations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { Child1Component } from './child1/child1.component';
 import { Child2Component } from './child2/child2.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,13 +23,10 @@ import { Child2Component } from './child2/child2.component';
     LoginComponent,
     HomeComponent,
     Child1Component,
-    Child2Component
+    Child2Component,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import * as data from '../user.json';
+import { FormControl, FormGroup } from '@angular/forms';
+import {
+  Validators,
+  FormBuilder,
+  ValidatorFn,
+  ValidationErrors,
+  AbstractControl,
+} from '@angular/forms';
+
+export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const forbidden = nameRe.test(control.value);
+    return forbidden ? { forbiddenName: { value: control.value } } : null;
+  };
+}
 
 @Component({
   selector: 'app-login',
@@ -8,7 +24,27 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent implements OnInit {
   isLogged!: boolean;
-  constructor(private authService: AuthService) {}
+
+  // loginForm = new FormGroup({
+  //   name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+  //   password: new FormControl('', [
+  //     Validators.required,
+  //     Validators.minLength(5),
+  //   ]),
+  // });
+
+  loginForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(5)]],
+  });
+
+  // loginForm = {
+  //   name: '',
+  //   password: '',
+  // };
+
+  constructor(private authService: AuthService, private fb: FormBuilder) {}
   ngOnInit(): void {
     this.authService.isAuthenticated().subscribe((isLogged) => {
       this.isLogged = isLogged;
@@ -21,5 +57,25 @@ export class LoginComponent implements OnInit {
 
   logout() {
     this.authService.logout();
+  }
+
+  onsubmit() {
+    const enteredEmail = this.loginForm.value.email;
+    const enteredPassword = this.loginForm.value.password;
+    const userData: { [key: string]: string } = data;
+    if (
+      enteredEmail! in userData &&
+      userData[enteredEmail!] === enteredPassword
+    ) {
+      this.authService.login();
+      window.alert('successfully lgged In');
+    } else {
+      console.log('Invalid credentials');
+      window.alert('Invalid username or passwords');
+    }
+    //   this.loginForm = {
+    //     name: '',
+    //     password: '',
+    //   };
   }
 }
